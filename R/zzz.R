7b146abe2de67f3dbbd0e782cd37beb7ee21734e
kgori <kevin.c.gori@gmail.com>
2017-10-27 11:35:05 +0100

    Fix missing imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 # .onLoad <- function(libname, pkgname) { Rcpp::loadRcppModules() }
+#' @importFrom "Rcpp" loadModule
 .onLoad <- function(libname, pkgname) {
   modules <- paste0("stan_fit4", names(stanmodels), "_mod")
   for (m in modules) loadModule(m, what = TRUE)
