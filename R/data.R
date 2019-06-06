7d30263792c0d27d19cb827576072370fc95e0e7
kgori <kevin.c.gori@gmail.com>
2019-06-06 12:24:50 +0100

    Add documentation for new data items

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Single base substitution signatures from COSMIC v3
+#'
+#' A data set of 67 signatures extracted by SigProfiler as described
+#' in Alexandrov, Kim, Hardhvala, Huang, et al., 2018.
+#' Signatures 51-67 are annotated as "Possible sequencing artefacts"
+#'
+#' @source \url{https://cancer.sanger.ac.uk/cosmic/signatures/SBS/}
+#' @source \url{https://www.synapse.org/#!Synapse:syn11801889}
+"cosmic_signatures_v3"
+
+#' Single base substitution signatures from COSMIC v3 with strand bias
+#'
+#' A data set of 67 strand bias signatures extracted by SigProfiler as described
+#' in Alexandrov, Kim, Hardhvala, Huang, et al., 2018.
+#' Signatures 51-67 are annotated as "Possible sequencing artefacts"
+#'
+#' @source \url{https://cancer.sanger.ac.uk/cosmic/signatures/SBS/}
+#' @source \url{https://www.synapse.org/#!Synapse:syn11801889}
+"cosmic_signatures_v3_strand"
+
