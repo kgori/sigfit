646cdf159129998b4ceb85dae00e58e0b74bec6d
baezortega <adrianbaez91@gmail.com>
2019-11-05 23:41:13 +0000

    Update vignette: added multichain extraction example

--- doc/sigfit_vignette.R
+++ doc/sigfit_vignette.R
@@ -7,6 +7,7 @@ par(mar = c(6, 4, 6, 4))
 #  devtools::install_github("kgori/sigfit", build_opts = c("--no-resave-data", "--no-manual"))
 
 ## ----fetch---------------------------------------------------------------
+library(sigfit)
 data("cosmic_signatures_v2")
 
 ## ----sim-----------------------------------------------------------------
@@ -17,46 +18,47 @@ colnames(mutations) <- colnames(cosmic_signatures_v2)
 
 ## ----plotsim, fig.width=20, fig.height=7, out.width="100%", echo=-1------
 par(mar = c(4.5,5.5,6.5,1))
-sigfit::plot_spectrum(mutations, name = "Simulated counts")
+plot_spectrum(mutations, name = "Simulated counts")
 
 ## ----fitting, warning=FALSE----------------------------------------------
-mcmc_samples_fit <- sigfit::fit_signatures(counts = mutations, 
-                                           signatures = cosmic_signatures_v2,
-                                           iter = 2000, 
-                                           warmup = 1000, 
-                                           chains = 1, 
-                                           seed = 1756)
+mcmc_samples_fit <- fit_signatures(counts = mutations, 
+                                   signatures = cosmic_signatures_v2,
+                                   iter = 2000, 
+                                   warmup = 1000, 
+                                   chains = 1, 
+                                   seed = 1756)
 
 ## ----retrieve_exp--------------------------------------------------------
-exposures <- sigfit::retrieve_pars(mcmc_samples_fit, 
-                                   par = "exposures", 
-                                   hpd_prob = 0.90)
+exposures <- retrieve_pars(mcmc_samples_fit, 
+                           par = "exposures", 
+                           hpd_prob = 0.90)
 names(exposures)
 exposures$mean
 
 ## ----plot_exp, fig.width=17, fig.height=7, out.width='100%', fig.align="center", echo=-1----
 par(mar=c(8,5,3.5,0))
-sigfit::plot_exposures(mcmc_samples = mcmc_samples_fit)
+plot_exposures(mcmc_samples = mcmc_samples_fit)
 
 ## ----refitting, eval=FALSE-----------------------------------------------
 #  selected_signatures <- c(1, 3, 7, 11)
-#  mcmc_samples_fit_2 <- sigfit::fit_signatures(mutations,
-#                                               cosmic_signatures_v2[selected_signatures, ],
-#                                               iter = 2000,
-#                                               warmup = 1000,
-#                                               chains = 1,
-#                                               seed = 1756)
+#  mcmc_samples_fit_2 <- fit_signatures(mutations,
+#                                       cosmic_signatures_v2[selected_signatures, ],
+#                                       iter = 2000,
+#                                       warmup = 1000,
+#                                       chains = 1,
+#                                       seed = 1756)
 
 ## ----reconstruct, fig.width=25, fig.height=18.5, out.width='100%', warning=FALSE, results="hide", echo=-1----
 par(mar=c(5,6,6.5,1))
-sigfit::plot_reconstruction(mcmc_samples = mcmc_samples_fit)
+plot_reconstruction(mcmc_samples = mcmc_samples_fit)
 
 ## ----plot_all, eval=FALSE------------------------------------------------
-#  sigfit::plot_all(mcmc_samples = mcmc_samples_fit,
-#                   out_path = "your/output/dir/here",
-#                   prefix = "Fitting")
+#  plot_all(mcmc_samples = mcmc_samples_fit,
+#           out_path = "your/output/dir/here",
+#           prefix = "Fitting")
 
 ## ----load_mutations------------------------------------------------------
+library(sigfit)
 data("variants_21breast")
 head(variants_21breast)
 
@@ -64,20 +66,20 @@ head(variants_21breast)
 unique(variants_21breast[, 1])
 
 ## ----build_catalogues----------------------------------------------------
-counts_21breast <- sigfit::build_catalogues(variants_21breast)
+counts_21breast <- build_catalogues(variants_21breast)
 dim(counts_21breast)
 counts_21breast[1:5, 1:8]
 
 ## ----plot_spectra, fig.width=22, fig.height=23, out.width='100%', fig.align="center", echo=-1----
 par(mar = c(5,6,7,2))
 par(mfrow = c(7, 3))
-sigfit::plot_spectrum(counts_21breast)
+plot_spectrum(counts_21breast)
 
 ## ----extraction, eval=FALSE----------------------------------------------
-#  mcmc_samples_extr <- sigfit::extract_signatures(counts = counts_21breast,
-#                                                  nsignatures = 2:7,
-#                                                  iter = 1000,
-#                                                  seed = 1756)
+#  mcmc_samples_extr <- extract_signatures(counts = counts_21breast,
+#                                          nsignatures = 2:7,
+#                                          iter = 1000,
+#                                          seed = 1756)
 
 ## ----plot_gof_silent, echo=FALSE, fig.width=9, fig.height=6, out.width="100%"----
 ## Plot precalculated GOF in order to avoid running the model
@@ -98,8 +100,8 @@ signatures <- extr_signatures
 
 ## ----retrieve_sigs, eval=FALSE-------------------------------------------
 #  ## Note: mcmc_samples_extr[[N]] contains the extraction results for N signatures
-#  signatures <- sigfit::retrieve_pars(mcmc_samples_extr[[4]],
-#                                      par = "signatures")
+#  signatures <- retrieve_pars(mcmc_samples_extr[[4]],
+#                              par = "signatures")
 
 ## ----show_signames-------------------------------------------------------
 rownames(signatures$mean)
@@ -107,11 +109,11 @@ rownames(signatures$mean)
 ## ----plot_sigs, warning=FALSE, fig.width=25, fig.height=10, out.width='100%', fig.align="center", echo=-1----
 par(mar = c(6,7,6,1))
 par(mfrow = c(2, 2))
-sigfit::plot_spectrum(signatures)
+plot_spectrum(signatures)
 
 ## ----match_sigs----------------------------------------------------------
 data("cosmic_signatures_v2")
-sigfit::match_signatures(signatures, cosmic_signatures_v2)
+match_signatures(signatures, cosmic_signatures_v2)
 
 ## ----refitting2, eval=FALSE----------------------------------------------
 #  signatures <- retrieve_pars(mcmc_samples_extr_2, "signatures")
@@ -136,25 +138,50 @@ plot_spectrum(counts, name = "Arbitrary catalogue")
 ## ----convert_sigs, eval=F------------------------------------------------
 #  # (Following from the signature extraction example presented above)
 #  # Retrieve genome-derived signatures
-#  genome_signatures <- sigfit::retrieve_pars(mcmc_samples_extr[[4]],
-#                                             par = "signatures")
+#  genome_signatures <- retrieve_pars(mcmc_samples_extr[[4]],
+#                                     par = "signatures")
 #  
 #  # Apply exome mutational opportunities
-#  exome_signatures <- sigfit::convert_signatures(genome_signatures,
-#                                                 opportunities_from = "human-genome",
-#                                                 opportunities_to = "human-exome")
+#  exome_signatures <- convert_signatures(genome_signatures,
+#                                         opportunities_from = "human-genome",
+#                                         opportunities_to = "human-exome")
 #  
 #  par(mfrow = c(2, 1))
-#  sigfit::plot_spectrum(genome_signatures$mean[4,],
-#                        name = "Signature D, Genome-relative probabilities")
-#  sigfit::plot_spectrum(exome_signatures[4,],
-#                        name = "Signature D, Exome-relative probabilities")
+#  plot_spectrum(genome_signatures$mean[4,],
+#                name = "Signature D, Genome-relative probabilities")
+#  plot_spectrum(exome_signatures[4,],
+#                name = "Signature D, Exome-relative probabilities")
 
 ## ----convert_sigs_silent, echo=F, fig.width=22, fig.height=14, out.width="100%"----
-exome_signatures <- sigfit::convert_signatures(signatures, opportunities_from = "human-genome", opportunities_to = "human-exome")
+exome_signatures <- convert_signatures(signatures, opportunities_from = "human-genome", opportunities_to = "human-exome")
 par(mfrow = c(2, 1), mar = c(5,5.5,6.5,1))
-sigfit::plot_spectrum(signatures$mean[4,], name="Signature D, Genome-relative probabilities")
-sigfit::plot_spectrum(exome_signatures[4,], name="Signature D, Exome-relative probabilities")
+plot_spectrum(signatures$mean[4,], name="Signature D, Genome-relative probabilities")
+plot_spectrum(exome_signatures[4,], name="Signature D, Exome-relative probabilities")
+
+## ----multichain, eval=F--------------------------------------------------
+#  # Load mutation counts
+#  data("counts_21breast")
+#  
+#  # Set number of cores for multi-chain sampling
+#  ncores <- parallel::detectCores()
+#  options(mc.cores = ncores)
+#  
+#  # Run a short single chain to find initial parameter values
+#  ext_init <- extract_signatures(counts_21breast,
+#                                 nsignatures = 4,
+#                                 iter = 1000,
+#                                 chains = 1)
+#  
+#  # Obtain a list of initial parameter values
+#  inits <- get_initializer_list(ext_init, chains = ncores)
+#  
+#  # Run multiple chains in parallel, initialised at the values found in the short run
+#  # (make sure to use the same number of chains as in `get_initializer_list`)
+#  ext_multichain <- extract_signatures(counts_21breast,
+#                                       nsignatures = 4,
+#                                       iter = 5000,
+#                                       chains = ncores,
+#                                       init = inits)
 
 ## ------------------------------------------------------------------------
     data("cosmic_signatures_v2")
